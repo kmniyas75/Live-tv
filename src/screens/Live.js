784e738f92dc528630a2187a6fa8e5a6f6762d2f
Niyas km <101791903+kmniyas75@users.noreply.github.com>
2022-06-16 14:46:26 +0530

    Update Live.js

--- src/screens/Live.js
+++ src/screens/Live.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import ReactPlayer from 'react-player'
 
 export default function Live() {
   return (
-    <div>Live</div>
+    <div className='liveScreen'>
+      <ReactPlayer  controls={true} playing={true} url='http://103.199.161.254/Content/asianetnews/Live/Channel(Asianetnews)/index.m3u8' />
+    </div>
   )
 }
